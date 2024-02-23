131ab8195fd754cb00ffb47651487e2940e474b4
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-02-23 23:18:02 +0530

    Add loader functionality

--- src/user/admin.py
+++ src/user/admin.py
@@ -1,5 +1,4 @@
 from sqladmin import ModelView
-from src.todo_list.schema import ListItem
 from src.user.schema import User
 
 
@@ -7,3 +6,5 @@ class UserView(ModelView, model=User):
     __tablename__ = "user"
 
     column_list = [User.id, User.email, User.is_active, User.is_superuser, User.is_verified]
+    # can_create = False
+    # can_delete = False
